8437bad0a941497368fce00717432006bad0cafe
jayanthkrishna <gannerla16038@iiitd.ac.in>
2023-01-04 23:14:26 +0530

    Added Pub Sub

--- main.go
+++ main.go
@@ -1,31 +1,38 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
-	"time"
 
 	"github.com/go-redis/redis/v8"
 )
 
 func main() {
+
 	client := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "",
 		DB:       0,
 	})
 
-	s := NewStore(NewRedisCache(client, time.Second*4))
-
-	for q := 0; q < 2; q++ {
-		val, err := s.Get(1)
-
-		if err != nil {
+	ctx := context.Background()
+	for i := 0; i < 100; i++ {
+		if err := client.Publish(ctx, "coords", i).Err(); err != nil {
 			log.Fatal(err)
 		}
-
-		fmt.Println(val)
-		time.Sleep(5 * time.Second)
 	}
+
+	// s := NewStore(NewRedisCache(client, time.Second*4))
+
+	// for q := 0; q < 2; q++ {
+	// 	val, err := s.Get(1)
+
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+
+	// 	fmt.Println(val)
+	// 	time.Sleep(5 * time.Second)
+	// }
 
 }
